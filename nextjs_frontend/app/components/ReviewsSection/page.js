56bf1388f58227417c9d971432fea3cecafd98d0
anthenny <Anthenny1997@hotmail.com>
2023-09-05 02:10:55 +0200

    [ verzoek ] navbar aangepast, layoutjs aangepast

--- nextjs_frontend/app/components/ReviewsSection/page.js
+++ nextjs_frontend/app/components/ReviewsSection/page.js
@@ -1,62 +1,115 @@
+'use client'
 import Image from 'next/image';
 import React from 'react';
+import {useEffect} from "react";
+import TinySlider from 'tiny-slider-react'
+import "tiny-slider/dist/tiny-slider.css";
 
 import arrow2 from '../../../public/icon/arrow2.png';
 
+const settings = {
+    lazyload: true,
+  nav: false,
+  controls: true,
+  mouseDrag: true,
+  loop: false,
+  items: 3,
+  gutter: 20,
+};
+
+const clickEvent = (slide) => {
+  console.log(slide);
+};
+
+
+
 const page = () => {
+
   return (
     <div className="container fwRewiew">
-      <h2 className="section__title">revieuws</h2>
+      <h2 className="section__title">Reviews</h2>
 
-      <div className="slider">
-        <div className="slids">
+      {/*<div className="slider">*/}
+      {/*  <div className="slids">*/}
+          {/*Arrow*/}
+          <div>
+            <Image src={arrow2} alt="arrow icon" width={50} height={50} />
+          </div>
+        
+          {/*card item*/}
+          <TinySlider settings={settings} onInit={clickEvent}>
+              <div className="wrapper">
+                  <div className="reviewsCard">
+                    <p>Anthenny de Hoon</p>
+                    <p>28-06-2023</p>
+                    <p>
+                      “Een goed uitgedacht schema met heerlijke gerechten. Daarnaast waren de HIIT
+                      trainingen een leuke aanvulling op het schema.”
+                    </p>
+                    <div className="line__col">
+                      <span>//</span>
+                      <div className="line" />
+                    </div>
+                    <p>9 / 10</p>
+                  </div>
+              </div>
+
+              <div className="wrapper">
+                  <div className="reviewsCard">
+                      <p>Anthenny de Hoon</p>
+                      <p>28-06-2023</p>
+                      <p>
+                          “Een goed uitgedacht schema met heerlijke gerechten. Daarnaast waren de HIIT
+                          trainingen een leuke aanvulling op het schema.”
+                      </p>
+                      <div className="line__col">
+                          <span>//</span>
+                          <div className="line" />
+                      </div>
+                      <p>9 / 10</p>
+                  </div>
+              </div>
+
+              <div className="wrapper">
+                  <div className="reviewsCard">
+                      <p>Anthenny de Hoon</p>
+                      <p>28-06-2023</p>
+                      <p>
+                          “Een goed uitgedacht schema met heerlijke gerechten. Daarnaast waren de HIIT
+                          trainingen een leuke aanvulling op het schema.”
+                      </p>
+                      <div className="line__col">
+                          <span>//</span>
+                          <div className="line" />
+                      </div>
+                      <p>9 / 10</p>
+                  </div>
+              </div>
+
+              <div className="wrapper">
+                  <div className="reviewsCard">
+                      <p>Anthenny de Hoon</p>
+                      <p>28-06-2023</p>
+                      <p>
+                          “Een goed uitgedacht schema met heerlijke gerechten. Daarnaast waren de HIIT
+                          trainingen een leuke aanvulling op het schema.”
+                      </p>
+                      <div className="line__col">
+                          <span>//</span>
+                          <div className="line" />
+                      </div>
+                      <p>9 / 10</p>
+                  </div>
+              </div>
+
+          </TinySlider>
+
+          {/*next arrow*/}
           <span>
             <Image src={arrow2} alt="arrow icon" width={50} height={50} />
           </span>
-          <div className="reviewsCard">
-            <p>Anthenny de Hoon</p>
-            <p>28-06-2023</p>
-            <p>
-              “Een goed uitgedacht schema met heerlijke gerechten. Daarnaast waren de HIIT
-              trainingen een leuke aanvulling op het schema.”
-            </p>
-            <div className="line__col">
-              <span>//</span>
-              <div className="line" />
-            </div>
-            <p>9 / 10</p>
-          </div>
-          <div className="reviewsCard">
-            <p>Anthenny de Hoon</p>
-            <p>28-06-2023</p>
-            <p>
-              “Een goed uitgedacht schema met heerlijke gerechten. Daarnaast waren de HIIT
-              trainingen een leuke aanvulling op het schema.”
-            </p>
-            <div className="line__col">
-              <span>//</span>
-              <div className="line" />
-            </div>
-            <p>9 / 10</p>
-          </div>
-          <div className="reviewsCard">
-            <p>Anthenny de Hoon</p>
-            <p>28-06-2023</p>
-            <p>
-              “Een goed uitgedacht schema met heerlijke gerechten. Daarnaast waren de HIIT
-              trainingen een leuke aanvulling op het schema.”
-            </p>
-            <div className="line__col">
-              <span>//</span>
-              <div className="line" />
-            </div>
-            <p>9 / 10</p>
-          </div>
-          <span>
-            <Image src={arrow2} alt="arrow icon" width={50} height={50} />
-          </span>
-        </div>
-      </div>
+      {/*  </div>*/}
+      {/*</div>*/}
 
       <div className="cReview">
         <p>
