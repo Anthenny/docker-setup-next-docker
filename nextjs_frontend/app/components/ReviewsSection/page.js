f4c2089e92ae56e9df93357cf469487765392b89
Olaf <nirvanaww7@gmail.com>
2023-09-06 12:49:22 +0200

    sliders aangepast

--- nextjs_frontend/app/components/ReviewsSection/page.js
+++ nextjs_frontend/app/components/ReviewsSection/page.js
@@ -10,30 +10,38 @@ const settings = {
   lazyload: true,
   nav: false,
   controls: true,
-  controlsContainer: '#custom',
-  prevButton: '.prev',
-  nextButton: '.next',
+  controlsContainer: '#custom_reviews',
+  prevButton: '.prev_reviews',
+  nextButton: '.next_reviews',
   mouseDrag: true,
   loop: true,
-  items: 3,
+  items: 1,
   gutter: 20,
+  responsive: {
+    1200: {
+      items: 3, // Aantal items op breder scherm
+    },
+    800: {
+      items: 2, // Aantal items op breder scherm
+    },
+  },
 };
-
-const clickEvent = (slide) => {};
 
 const page = () => {
   return (
     <div className="container fwRewiew">
       <h2 className="section__title">Reviews</h2>
-      <ul className="custom-controls" id={'custom'}>
-        <li className={'prev'}>
+
+      <ul className="custom-controls" id={'custom_reviews'}>
+        <li className={'prev_reviews'}>
           <Image src={arrow2} alt="arrow icon" width={50} height={50} />
         </li>
-        <li className={'next'}>
+        <li className={'next_reviews'}>
           <Image src={arrow2} alt="arrow icon" width={50} height={50} />
         </li>
       </ul>
-      <TinySlider settings={settings} onInit={clickEvent}>
+
+      <TinySlider settings={settings}>
         <div className="wrapper">
           <div className="reviewsCard">
             <p>Anthenny de Hoon</p>
@@ -98,6 +106,7 @@ const page = () => {
           </div>
         </div>
       </TinySlider>
+
       <div className="cReview">
         <p>
           Toensiefitness is gemiddeld beoordeeld met een 9.1 / 10 op basis van 920 klantervaringen.
--- nextjs_frontend/app/layout.js
+++ nextjs_frontend/app/layout.js
@@ -17,12 +17,13 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <Head>
         <title>Toensie Fitness</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin></link>
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;700&family=Roboto:wght@400;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Istok+Web:ital,wght@1,700&family=Poppins:wght@400;700&family=Roboto:wght@400;700&display=swap"
           rel="stylesheet"
-        ></link>
+        />
       </Head>
       <body>
         <Nav />
