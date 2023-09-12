ce3f1d67018b881cc95b9b26e870d390e545300a
anthenny <Anthenny1997@hotmail.com>
2023-09-13 00:51:51 +0200

    [ verzoek ] instagram sectie moet hover effect bij

--- nextjs_frontend/app/components/InstagramSection/page.js
+++ nextjs_frontend/app/components/InstagramSection/page.js
@@ -1,35 +1,30 @@
 import React from 'react';
-import Image from 'next/image';
-import instagramIcon from '@/../../public/icon/instagram.png';
+import Image from "next/image";
 
 const page = () => {
-  const slides = [
-    { imageUrl: '/../public/img/mainImag.jpg' },
-    { imageUrl: '/../public/img/T1_img.jpg' },
-    { imageUrl: '/../public/img/mainImag.jpg' },
-    { imageUrl: '/../public/img/T4_img.jpg' },
-  ];
-  return (
-    <div className="container fwInstagram">
-      <h2 className="section__title">INSTAGRAM</h2>
-      <div className="slider">
-        <div className="slids">
-          {slides.map((slid, index) => {
-            return (
-              <div className="slid" key={index} style={{ height: 267 }}>
-                <Image src={slid.imageUrl} fill alt="instagram section image" sizes="100vw" />
-                <span>
-                  <Image src={instagramIcon} width={40} height={40} alt="icon instagram" />
-                </span>
-                <div className="darkBg" />
-                <div className="darkBg2" />
-              </div>
-            );
-          })}
+    const images = [
+        {imageUrl: '/../public/img/mainImag.jpg'},
+        {imageUrl: '/../public/img/T1_img.jpg'},
+        {imageUrl: '/../public/img/mainImag.jpg'},
+        {imageUrl: '/../public/img/T4_img.jpg'},
+    ];
+
+    return (
+        <div className="fullwidth fwInstagram">
+            <div className="container cInstagram">
+                <h2>instagram</h2>
+                <div className="images">
+                    {images.map((image, key) => {
+                        return (
+                            <div className="image" key={key}>
+                                <Image src={image.imageUrl} alt="test" width={300} height={300}/>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default page;
